23cd38eefcc69fa372a5fe39fc676652afab293d
Lucas Bonafé <31395135+LucasBonafe@users.noreply.github.com>
2017-09-26 23:01:11 -0300

    Optimized Tabuada.cs
    Reduction from 26 to 19 lines of code.

--- Tabuada.cs
+++ Tabuada.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 namespace ConsoleApplication1{
@@ -9,17 +8,11 @@ namespace ConsoleApplication1{
         static void Main(string[] args){
             Console.WriteLine("\nTABUADA MULTIPLICAÇÃO\n");
             for (int n1 = 1, n2 = 1; n2 < 11;){
-                Console.Write(n1 + "x" + n2 + "=" + (n1 * n2) + "\t"); n1++;//1
-                Console.Write(n1 + "x" + n2 + "=" + (n1 * n2) + "\t"); n1++;//2
-                Console.Write(n1 + "x" + n2 + "=" + (n1 * n2) + "\t"); n1++;//3
-                Console.Write(n1 + "x" + n2 + "=" + (n1 * n2) + "\t"); n1++;//4
-                Console.Write(n1 + "x" + n2 + "=" + (n1 * n2) + "\t"); n1++;//5
-                Console.Write(n1 + "x" + n2 + "=" + (n1 * n2) + "\t"); n1++;//6
-                Console.Write(n1 + "x" + n2 + "=" + (n1 * n2) + "\t"); n1++;//7
-                Console.Write(n1 + "x" + n2 + "=" + (n1 * n2) + "\t"); n1++;//8
-                Console.Write(n1 + "x" + n2 + "=" + (n1 * n2) + "\t"); n1++;//9
-                Console.WriteLine(n1 + "x" + n2 + "=" + (n1 * n2)); n2++;//10
-                n1 = n1 - 9;
+                Console.Write(n1 + "x" + n2 + "=" + (n1 * n2) + "\t"); n1++;//9x
+                if (n1==10){
+                    Console.WriteLine(n1 + "x" + n2 + "=" + (n1 * n2)); n2++;//1x
+                    n1 = n1 - 9;
+                }
             }
             Console.WriteLine("\nDigite qualquer tecla para sair.");
             Console.ReadKey();
