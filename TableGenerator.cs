f7d1487b3be92ca41cce1db32e5b6359a724b604
Lucas Bonafé <31395135+LucasBonafe@users.noreply.github.com>
2017-09-28 22:51:25 -0300

    Update TableGenerator.cs
    String Interpolation changes...

--- TableGenerator.cs
+++ TableGenerator.cs
@@ -1,16 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace ConsoleApplication1{
-    class Program{
-        static void Main(string[] args){
-                Console.WriteLine("______________________________\nTabela:\n");
-            string[] nomes = new string[]{"José","Maria","Silvio","Ana","João","Juan","Lais"};
-            for (int num = 0; num < nomes.Length; num++){
-                String nome = nomes[num];
-                Console.WriteLine("Aluno nº" + (num+1) + "\t Nome: " + nome);}
-                Console.WriteLine("______________________________");
-            Console.ReadKey();
+namespace ConsoleApp1{
+	class Program{
+		static void Main(string[] args){
+			Console.WriteLine("______________________________\nTabela:\n");
+			string[] nomes = new string[] {"José", "Maria", "Silvio", "Ana", "João", "Juan", "Lais"};
+			for (int num = 0; num < nomes.Length; num++){
+				String nome = nomes[num];
+				Console.WriteLine($"Aluno nº{num+1}\t Nome: {nome}");
+			}
+Console.WriteLine("______________________________");
+Console.ReadKey();
 }}}
