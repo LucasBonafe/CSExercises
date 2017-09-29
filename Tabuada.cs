a35a289f97b83e1b57f0edc5694ff584a83964af
Lucas Bonafé <31395135+LucasBonafe@users.noreply.github.com>
2017-09-28 22:40:27 -0300

    Update Tabuada.cs
    String Interpolation changes...

--- Tabuada.cs
+++ Tabuada.cs
@@ -1,15 +1,15 @@
 using System;
-namespace Tabuada{
-    class Program{
+namespace ConsoleApp1{
+	class Program{
         static void Main(string[] args){
             Console.WriteLine("\nTABUADA MULTIPLICAÇÃO\n");
             for (int n1 = 1, n2 = 1; n2 < 11;){
-                Console.Write(n1 + "x" + n2 + "=" + (n1 * n2) + "\t"); n1++;//9x
-                if (n1==10){
-                    Console.WriteLine(n1 + "x" + n2 + "=" + (n1 * n2)); n2++;//1x
+                Console.Write($"{n1}x{n2}={n1*n2}\t"); n1++;//9x
+                if (n1 == 10){
+                    Console.WriteLine($"{n1}x{n2}={n1*n2}"); n2++;//1x
                     n1 = n1 - 9;
                 }
             }
-            Console.WriteLine("\nDigite qualquer tecla para sair.");
-            Console.ReadKey();
+Console.WriteLine("\nDigite qualquer tecla para sair.");
+Console.ReadKey();
 }}}
