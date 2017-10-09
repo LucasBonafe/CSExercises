b8696316b4e3ab556411f67ea6bf558f00c69311
Lucas Bonafé <31395135+LucasBonafe@users.noreply.github.com>
2017-10-09 18:48:38 -0300

    Update MaiorNota.cs
    String Interpolation...

--- MaiorNota.cs
+++ MaiorNota.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 namespace ConsoleApplication1{
     class Program{
         static void Main(string[] args){
@@ -10,11 +6,11 @@ namespace ConsoleApplication1{
             int num=0, nota=0, maior=0;
             while (num < alunos.Length){
                 String aluno = alunos[num];
-                Console.Write("Digite a nota do {0} aluno: ", aluno); num++;
+                Console.Write($"Digite a nota do {aluno} aluno: "); num++;
                 nota = Convert.ToInt32(Console.ReadLine());
                 if (nota > maior) {maior = nota;}
             }
-            Console.WriteLine("A maior nota foi: "+ maior);
+            Console.WriteLine($"A maior nota foi: {maior}");
             Console.WriteLine("\nDigite qualquer tecla para sair.");
             Console.ReadKey();
 }}}
